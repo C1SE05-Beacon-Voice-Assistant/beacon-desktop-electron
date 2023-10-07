2a1801674429116903661e52913d2437818b5867
Yensubldg <thang11a5tp@gmail.com>
2023-10-07 09:03:33 +0700

    update

--- src/components/sidebars/DefaultSidebar.tsx
+++ src/components/sidebars/DefaultSidebar.tsx
@@ -2,22 +2,19 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import { styled, useTheme } from "@mui/material/styles";
-import * as React from "react";
-import { useNavigate } from "react-router-dom";
 import HomeIcon from "~/components/icons/home-icon";
-import InforIcon from "~/components/icons/infor-icon";
+import TimeIcon from "~/components/icons/time-icon";
 import LogoIcon from "~/components/icons/logo-icon";
 import SettingIcon from "~/components/icons/setting-icon";
 import InforIcon from "~/components/icons/infor-icon";
-import TimeIcon from "~/components/icons/time-icon";
 import { AppRouter } from "~/constants/appRoutes";
 
 const drawerWidth = 240;
@@ -34,7 +31,7 @@ type SideBarLayoutProps = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
-export default function DefaultSidebar(props: SideBarLayoutProps) {
+export default function SideBarLayout(props: SideBarLayoutProps) {
   const { open, setOpen } = props;
   const theme = useTheme();
   const [selectedIndex, setSelectedIndex] = useState<number | null>(1);
