0d8b03fc149f0997e62bf1e128c5291fe0b0e212
bd500 <qbao1101@gmail.com>
2023-10-25 21:25:24 +0700

    handle exception

--- electron/utils/newsReader.js
+++ electron/utils/newsReader.js
@@ -14,8 +14,6 @@ class NewsReader {
                 console.log(element);
             } else content.push(element.children[0].data);
         });
-        // const p = mainContent.find("p");
-        // console.log(p[0].children[0]);
         fs.writeFile("electron/utils/news.txt", content.join("\n"), (err) =>
             console.log(err)
         );
@@ -24,15 +22,6 @@ class NewsReader {
 
     async photoNews(mainContent) {
         const content = [];
-
-        // mainContent.find("p").each((index, element) => {
-        //     if (element.children.length > 1) {
-        //         console.log(element.children);
-        //     } else if (element.children.length == 1) {
-        //         const data = element.children[0].data;
-        //         if (!content.includes(data)) content.push(data);
-        //     }
-        // });
 
         const desc_cation = mainContent
             .find("div.desc_cation")
@@ -100,8 +89,13 @@ class NewsReader {
             }
             // console.log(mainContent);
         } catch (err) {
-            console.log(err.response.status);
-            console.log(err.response.statusText);
+            if (err.response.status === 404)
+                throw new Error("Lỗi không tìm thấy trang");
+            else if (err.code === "ECONNREFUSED")
+                throw new Error("Không có kết nối mạng");
+            else if (err.response.status === 503)
+                throw new Error("Mất quá nhiều thời gian để phản hồi");
+            else throw new Error("Server đang bị lỗi, vui lòng thử lại sau");
         }
         return result;
     }
@@ -115,7 +109,7 @@ const urlLive = "https://vnexpress.net/asiad-ngay-4-10-4660546.html";
 const urlFail = "https://vnexpress.net/asiad-ngay-4--46606.html";
 const photoMidu = "https://vnexpress.net/sac-voc-midu-o-tuoi-34-4661302.html";
 
-new NewsReader().getNewsContent(photoMidu).then((res) => {
+new NewsReader().getNewsContent(urlFail).then((res) => {
     console.log(res);
 });
 
