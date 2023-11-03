3a78e93fca238e7fbaa390a08b369e1be9e9d2c5
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-11-04 01:57:58 +0700

    completed check status of internet

---
+++ electron/detect_internet_status.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const checkInternetConnection = (callback) => {
+  http
+    .get("http://www.google.com", (res) => {
+      callback(true);
+    })
+    .on("error", () => {
+      callback(false);
+    });
+};
+
+checkInternetConnection((isConnected) => {
+  if (isConnected) {
+    console.log("Máy tính đang kết nối internet.");
+  } else {
+    console.log("Máy tính không có kết nối internet.");
+  }
+
+  return isConnected;
+});
