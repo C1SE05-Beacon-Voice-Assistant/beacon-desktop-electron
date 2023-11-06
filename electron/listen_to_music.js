5eb1fb7dd6fc244a65bee56a6ae87687e58f4323
Than Thi Thao <thanthao051102@gmail.com>
2023-11-06 11:32:56 +0700

    error player not defind

--- electron/listen_to_music.js
+++ electron/listen_to_music.js
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { By } = require("selenium-webdriver");
+const { By, Builder } = require("selenium-webdriver");
+const driver = new Builder().forBrowser("chrome").build();
+const { exec } = require("child_process");
+// const player = require('play-sound')({player: "C:\Users\THAN THI THAO\Downloads\mplayer_10\MPlayer-1.0rc2-gui\MPlayer-1.0rc2-gui\gmplayer.exe"});
 
 async function listenToMusic(driver) {
   let platformName = null;
@@ -14,45 +17,58 @@ async function listenToMusic(driver) {
   }
 
   async function playOnYoutube() {
-    platformName = "youtube";
-    const url = `https://yewtu.be/search?q=${songName}`;
-    await driver.get(url);
-
-    const songList = await driver.findElements(By.className("video-card-row"));
-    if (songList.length > 0) {
-      await driver.sleep(1000);
-      await songList[0].click();
+    try {
+      platformName = "youtube";
+      const url = `https://yewtu.be/search?q=${songName}`;
+      await driver.get(url);
+  
+      const songList = await driver.findElements(By.className("video-card-row"));
+      if (songList.length > 0) {
+        await driver.sleep(1000);
+        await songList[0].click();
+      } else {
+        throw new Error("Không tìm thấy bài hát trên YouTube");
+      }
+    } catch (error) {
+      console.error("Lỗi xảy ra khi phát nhạc trên YouTube:", error);
+        // Trả về voice file âm thanh có sẵn trong máy
+      const voiceFilePath = "G:\\Electron\\beacon-desktop-electron\\foo.mp3";
+      player.play(voiceFilePath, (err) => {
+      if (err) {
+        console.error("Lỗi xảy ra khi phát âm thanh:", err);
+      }
+    });
     }
   }
 
   async function playOnMp3() {
-    platformName = "mp3";
-    const url = `https://zingmp3.vn/tim-kiem/tat-ca?q=${songName}`;
+      platformName = "mp3";
+      const url = `https://zingmp3.vn/tim-kiem/tat-ca?q=${songName}`;
 
-    await driver.get(url);
+      await driver.get(url);
 
-    const showPlayButtons = await driver.findElements(
-      By.css("div.zm-carousel-item")
-    );
-
-    if (showPlayButtons.length > 0) {
-      await driver.sleep(1000);
-      await showPlayButtons[0].click();
-
-      const playButton = await driver.findElements(
-        By.css("button.action-play")
+      const showPlayButtons = await driver.findElements(
+        By.css("div.zm-carousel-item")
       );
 
-      if (playButton.length > 0) {
+      if (showPlayButtons.length > 0) {
         await driver.sleep(1000);
-        await playButton[0].click();
-        return "Done";
+        await showPlayButtons[0].click();
+
+        const playButton = await driver.findElements(
+          By.css("button.action-play")
+        );
+
+        if (playButton.length > 0) {
+          await driver.sleep(1000);
+          await playButton[0].click();
+          return "Done";
+        } else {
+          throw new Error("No play button found");
+        }
       } else {
-        throw new Error("No play button found");
+        throw new Error("No show play buttons found");
       }
-    } else {
-      throw new Error("No show play buttons found");
-    }
   }
 
   async function toggle(isPlay = false) {
@@ -99,5 +115,10 @@ async function listenToMusic(driver) {
     next,
   };
 }
+
+listenToMusic(driver).then((res) => {
+  res.searchSong("")
+  res.playOnYoutube()
+})
 
 module.exports = listenToMusic;
