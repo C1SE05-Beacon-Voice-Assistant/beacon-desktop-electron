de73af7b03c50c45ce4372120924527fe6509417
Thang Truong <windev.thang@gmail.com>
2023-11-07 09:50:13 +0700

    tmp

--- electron/preload.js
+++ electron/preload.js
@@ -10,21 +10,41 @@ const ReadNewsController = require(path.join(
   "read_news_controller.js"
 ));
 const getAudioDevices = require(path.join(__dirname, "detect_device.js"));
-
+const { start } = require(path.join(__dirname, "start.js"));
 const beacon = new BeaconSpeech("Beacon", "Hanoi");
-const driver = new Builder().forBrowser("chrome").build();
 
-const beaconVolume = createBeaconVolume().then((result) => result);
-const listenToMusicWithDriver = listenToMusic(driver);
-const readNews = new ReadNewsController(driver);
-const searchNewsBy = readNews.search.bind(readNews);
-const selectOneToRead = readNews.selectOneToRead.bind(readNews);
+const init = async () => {
+  start()
+    .then((res) => {
+      if (res) {
+        console.log("Enter name");
+        const name = beacon.recognizeFromMicrophone();
+        console.log(name);
+      } else {
+        console.log("exist");
+      }
+    })
+    .then(() => {
+      const driver = new Builder().forBrowser("chrome").build();
 
-contextBridge.exposeInMainWorld("electron", {
-  backgroundListen: beacon.backgroundListen.bind(beacon),
-  stopBackgroundListen: beacon.stopBackgroundListen.bind(beacon),
-  beaconVolume,
-  listenToMusic: listenToMusicWithDriver,
-  readNews: { searchNewsBy, selectOneToRead },
-  getAudioDevices,
-});
+      const beaconVolume = createBeaconVolume().then((result) => result);
+      const listenToMusicWithDriver = listenToMusic(driver);
+      const readNews = new ReadNewsController(driver);
+      const searchNewsBy = readNews.search.bind(readNews);
+      const selectOneToRead = readNews.selectOneToRead.bind(readNews);
+
+      contextBridge.exposeInMainWorld("electron", {
+        backgroundListen: beacon.backgroundListen.bind(beacon),
+        stopBackgroundListen: beacon.stopBackgroundListen.bind(beacon),
+        beaconVolume,
+        listenToMusic: listenToMusicWithDriver,
+        readNews: { searchNewsBy, selectOneToRead },
+        getAudioDevices,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+init();
--- electron/start.js
+++ electron/start.js
@@ -1,15 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require("dotenv").config();
 const axios = require("axios");
-var os = require("os");
 const { machineIdSync } = require("node-machine-id");
 
 axios.defaults.baseURL = process.env.API_URL;
+const mac = machineIdSync({
+  original: true,
+});
+
+async function start() {
+  try {
+    const { data } = await axios.get(`/users/mac/${mac}`);
+    if (data) return;
+    else return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
 
 async function register(userInfo) {
-  const mac = machineIdSync({
-    original: true,
-  });
   const data = {
     ...userInfo,
     MAC: mac,
@@ -22,18 +32,19 @@ async function register(userInfo) {
     return error;
   }
 }
-register({
-  name: "test",
-  email: "test@test.com",
-  dob: "2023-11-04T15:24:14.035Z",
-  gender: true,
-  phone: "0123456789",
-})
-  .then((res) => {
-    console.log("pass", res);
-  })
-  .catch((err) => {
-    console.log("fail", err);
-  });
 
-module.exports = { register };
+// register({
+//   name: "test",
+//   email: "test@test.com",
+//   dob: "2023-11-04T15:24:14.035Z",
+//   gender: true,
+//   phone: "0123456789",
+// })
+//   .then((res) => {
+//     console.log("pass", res);
+//   })
+//   .catch((err) => {
+//     console.log("fail", err);
+//   });
+
+module.exports = { start, register };
