800a16cbe42a620310d9a0ef0bc8cce3cdd7f42a
Thang Truong <windev.thang@gmail.com>
2023-11-07 10:53:24 +0700

    Auto stash before merge of "except_for_application" and "origin/except_for_application"

--- electron/control_volume.js
+++ electron/control_volume.js
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const loudness = require("loudness");
-const { exec } = require('child_process');
-const player = require('play-sound')();
-
+const { exec } = require("child_process");
+const player = require("play-sound")();
 
 async function createBeaconVolume() {
   const volume = await loudness.getVolume();
@@ -35,78 +34,78 @@ async function createBeaconVolume() {
   }
 
   async function increaseVolume(number) {
-  try {
-    const newVolume = volume + number > 100 ? 100 : volume + number;
-    await setVolume(newVolume);
-  } catch (error) {
-    console.error("An error occurred while increasing the volume:", error);
-    playErrorSound();
-  }
-}
-
-async function decreaseVolume(number) {
-  try {
-    const newVolume = volume - number < 0 ? 0 : volume - number;
-    await setVolume(newVolume);
-  } catch (error) {
-    console.error("An error occurred while decreasing the volume:", error);
-    playErrorSound();
-  }
-}
-
-async function mute() {
-  try {
-    await loudness.setMuted(true);
-  } catch (error) {
-    console.error("An error occurred while muting the volume:", error);
-    playErrorSound();
-  }
-}
-
-async function unmute() {
-  try {
-    const mute = await loudness.getMuted();
-    if (mute) await loudness.setMuted(false);
-  } catch (error) {
-    console.error("An error occurred while unmuting the volume:", error);
-    playErrorSound();
-  }
-}
-
-// async function setVolumeToMax() {
-//   try {
-//     await setVolume(100);
-//   } catch (error) {
-//     console.error("An error occurred while setting the volume to maximum:", error);
-//     playErrorSound();
-//   }
-// }
-
-// async function setVolumeToMin() {
-//   try {
-//     await setVolume(0);
-//   } catch (error) {
-//     console.error("An error occurred while setting the volume to minimum:", error);
-//     playErrorSound();
-//   }
-// }
-async function setVolumeToMax() {
-  await setVolume(100);
-}
-
-async function setVolumeToMin() {
-  await setVolume(0);
-}
-
-function playErrorSound() {
-  const audioPath = "C:\\Users\\USER\\Documents\\beacon-desktop-electron\\Youtobe.mp3"; // Đường dẫn đến file âm thanh lỗi
-  const play = player.play(audioPath, (err) => {
-    if (err) {
-      console.error("An error occurred while playing the error sound:", err);
+    try {
+      const newVolume = volume + number > 100 ? 100 : volume + number;
+      await setVolume(newVolume);
+    } catch (error) {
+      console.error("An error occurred while increasing the volume:", error);
+      playErrorSound();
     }
-  });
-}
+  }
 
+  async function decreaseVolume(number) {
+    try {
+      const newVolume = volume - number < 0 ? 0 : volume - number;
+      await setVolume(newVolume);
+    } catch (error) {
+      console.error("An error occurred while decreasing the volume:", error);
+      playErrorSound();
+    }
+  }
+
+  async function mute() {
+    try {
+      await loudness.setMuted(true);
+    } catch (error) {
+      console.error("An error occurred while muting the volume:", error);
+      playErrorSound();
+    }
+  }
+
+  async function unmute() {
+    try {
+      const mute = await loudness.getMuted();
+      if (mute) await loudness.setMuted(false);
+    } catch (error) {
+      console.error("An error occurred while unmuting the volume:", error);
+      playErrorSound();
+    }
+  }
+
+  // async function setVolumeToMax() {
+  //   try {
+  //     await setVolume(100);
+  //   } catch (error) {
+  //     console.error("An error occurred while setting the volume to maximum:", error);
+  //     playErrorSound();
+  //   }
+  // }
+
+  // async function setVolumeToMin() {
+  //   try {
+  //     await setVolume(0);
+  //   } catch (error) {
+  //     console.error("An error occurred while setting the volume to minimum:", error);
+  //     playErrorSound();
+  //   }
+  // }
+  async function setVolumeToMax() {
+    await setVolume(100);
+  }
+
+  async function setVolumeToMin() {
+    await setVolume(0);
+  }
+
+  function playErrorSound() {
+    const audioPath =
+      "C:\\Users\\USER\\Documents\\beacon-desktop-electron\\Youtobe.mp3"; // Đường dẫn đến file âm thanh lỗi
+    const play = player.play(audioPath, (err) => {
+      if (err) {
+        console.error("An error occurred while playing the error sound:", err);
+      }
+    });
+  }
 
   return {
     setVolume,
