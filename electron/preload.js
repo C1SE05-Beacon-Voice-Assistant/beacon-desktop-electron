797cd2389d0441379c8edf3641d6c9e25ffe3146
Thang Truong <windev.thang@gmail.com>
2023-11-09 16:49:20 +0700

    fix some err, reduce size to 3xxMB done

--- electron/preload.js
+++ electron/preload.js
@@ -15,65 +15,70 @@ const ReadNewsController = require(path.join(
   "read_news_controller.js"
 ));
 const getAudioDevices = require(path.join(__dirname, "detect_device.js"));
-
-// const { start, register } = require(path.join(__dirname, "start.js"));
-// const beacon = new BeaconSpeech("Beacon", "Hanoi");
+const { checkInternetConnection } = require(path.join(
+  __dirname,
+  "detect_internet_status.js"
+));
+const executeException = require(path.join(__dirname, "situation_except.js"));
+const textToSpeech = require(path.join(__dirname, "text_to_speech.js"));
+const { start, register } = require(path.join(__dirname, "start.js"));
+const beacon = new BeaconSpeech("Beacon", "Hanoi");
 
 // process.env.API_URL = "http://localhost:8000/api";
 
-console.log("cuoi1");
-const init = async () => {
-  checkInternetConnection((isConnected) => {
+const init = () => {
+  checkInternetConnection(async (isConnected) => {
     if (isConnected) {
       console.error("Máy tính đang kết nối internet.");
     } else {
       console.log("Máy tính không có kết nối internet.");
       executeException("noInternet");
     }
-    //   await textToSpeech("Xin chào, tôi là Beacon, tôi có thể giúp gì cho bạn?");
-    //   start()
-    //     .then(async (res) => {
-    //       if (res) {
-    //         console.log("exist");
-    //         return true;
-    //       } else {
-    //         await textToSpeech("Hãy đăng ký thông tin của bạn");
-    //         await textToSpeech("Nhập tên");
-    //         const name = await beacon.recognizeFromMicrophone();
-    //         await textToSpeech("Nhập số điện thoại");
-    //         const phone = await beacon.recognizeFromMicrophone();
-    //         const userInfo = {
-    //           name,
-    //           phone,
-    //         };
-    //         return register(userInfo);
-    //       }
-    //     })
-    //     .then((res) => {
-    //       console.log(res);
-    //       const serviceBuilder = new ServiceBuilder(chromedriverPath);
-    //       const driver = new Builder()
-    //         .forBrowser("chrome")
-    //         .setChromeService(serviceBuilder)
-    //         .build();
+    await textToSpeech("Xin chào, tôi là Beacon, tôi có thể giúp gì cho bạn?");
+    start()
+      .then(async (res) => {
+        if (res) {
+          console.log("exist");
+          return true;
+        } else {
+          await textToSpeech("Hãy đăng ký thông tin của bạn");
+          await textToSpeech("Nhập tên");
+          const name = await beacon.recognizeFromMicrophone();
+          await textToSpeech("Nhập số điện thoại");
+          const phone = await beacon.recognizeFromMicrophone();
+          const userInfo = {
+            name,
+            phone,
+          };
+          return register(userInfo);
+        }
+      })
+      .then((res) => {
+        console.log(res);
+        const serviceBuilder = new ServiceBuilder(chromedriverPath);
+        const driver = new Builder()
+          .forBrowser("chrome")
+          .setChromeService(serviceBuilder)
+          .build();
 
-    //       const beaconVolume = createBeaconVolume().then((result) => result);
-    //       const listenToMusicWithDriver = listenToMusic(driver);
-    //       const readNews = new ReadNewsController(driver);
-    //       const searchNewsBy = readNews.search.bind(readNews);
-    //       const selectOneToRead = readNews.selectOneToRead.bind(readNews);
+        const beaconVolume = createBeaconVolume().then((result) => result);
+        const listenToMusicWithDriver = listenToMusic(driver);
+        const readNews = new ReadNewsController(driver);
+        const searchNewsBy = readNews.search.bind(readNews);
+        const selectOneToRead = readNews.selectOneToRead.bind(readNews);
 
-    //       contextBridge.exposeInMainWorld("electron", {
-    //         backgroundListen: beacon.backgroundListen.bind(beacon),
-    //         stopBackgroundListen: beacon.stopBackgroundListen.bind(beacon),
-    //         beaconVolume,
-    //         listenToMusic: listenToMusicWithDriver,
-    //         readNews: { searchNewsBy, selectOneToRead },
-    //         getAudioDevices,
-    //       });
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
+        contextBridge.exposeInMainWorld("electron", {
+          backgroundListen: beacon.backgroundListen.bind(beacon),
+          stopBackgroundListen: beacon.stopBackgroundListen.bind(beacon),
+          beaconVolume,
+          listenToMusic: listenToMusicWithDriver,
+          readNews: { searchNewsBy, selectOneToRead },
+          getAudioDevices,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 };
 
