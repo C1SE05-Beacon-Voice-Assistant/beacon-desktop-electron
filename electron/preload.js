9f6a1f013a909676818ca6c65f8bf77807d8e7d5
Thang Truong <windev.thang@gmail.com>
2023-11-09 16:32:12 +0700

    remove err

--- electron/preload.js
+++ electron/preload.js
@@ -24,17 +24,16 @@ const executeException = require(path.join(__dirname, "situation_except.js"));
 // const { start, register } = require(path.join(__dirname, "start.js"));
 // const beacon = new BeaconSpeech("Beacon", "Hanoi");
 
-
 // process.env.API_URL = "http://localhost:8000/api";
 
-console.log("cuoi1")
+console.log("cuoi1");
 const init = async () => {
   checkInternetConnection((isConnected) => {
     if (isConnected) {
       console.error("Máy tính đang kết nối internet.");
     } else {
       console.log("Máy tính không có kết nối internet.");
-      executeException('noInternet')
+      executeException("noInternet");
     }
     //   await textToSpeech("Xin chào, tôi là Beacon, tôi có thể giúp gì cho bạn?");
     //   start()
@@ -84,35 +83,3 @@ const init = async () => {
 };
 
 init();
-
-          return register(userInfo);
-        }
-      })
-      .then((res) => {
-        console.log(res);
-        const serviceBuilder = new ServiceBuilder(chromedriverPath);
-        const driver = new Builder()
-          .forBrowser("chrome")
-          .setChromeService(serviceBuilder)
-          .build();
-
-        const beaconVolume = createBeaconVolume().then((result) => result);
-        const listenToMusicWithDriver = listenToMusic(driver);
-        const readNews = new ReadNewsController(driver);
-        const searchNewsBy = readNews.search.bind(readNews);
-        const selectOneToRead = readNews.selectOneToRead.bind(readNews);
-
-        contextBridge.exposeInMainWorld("electron", {
-          backgroundListen: beacon.backgroundListen.bind(beacon),
-          stopBackgroundListen: beacon.stopBackgroundListen.bind(beacon),
-          beaconVolume,
-          listenToMusic: listenToMusicWithDriver,
-          readNews: { searchNewsBy, selectOneToRead },
-          getAudioDevices,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-};
