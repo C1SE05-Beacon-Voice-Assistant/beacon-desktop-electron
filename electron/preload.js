2e33d66658194ae1898a4e744794bb498cf63b10
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-11-11 02:13:25 +0700

    resolve conflict

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+require("dotenv").config();
 const { Builder } = require("selenium-webdriver");
 const { contextBridge, ipcRenderer } = require("electron");
 const chromedriverPath = require("chromedriver").path.replace(
@@ -7,8 +8,8 @@ const chromedriverPath = require("chromedriver").path.replace(
 );
 const path = require("path");
 const { ServiceBuilder } = require("selenium-webdriver/chrome");
-const { excute_intent } = require("./excute_intent");
-// const BeaconSpeech = require(path.join(__dirname, "beacon_speech.js"));
+const { execute_intent } = require(path.join(__dirname, "execute_intent"));
+const BeaconSpeech = require(path.join(__dirname, "beacon_speech.js"));
 const createBeaconVolume = require(path.join(__dirname, "control_volume.js"));
 const listenToMusic = require(path.join(__dirname, "listen_to_music.js"));
 const controlVolume = require(path.join(__dirname, "control_volume.js"));
@@ -17,32 +18,65 @@ const ReadNewsController = require(path.join(
   "read_news_controller.js"
 ));
 const getAudioDevices = require(path.join(__dirname, "detect_device.js"));
+const textToSpeech = require(path.join(__dirname, "text_to_speech.js"));
 const serviceBuilder = new ServiceBuilder(chromedriverPath);
-// const beacon = new BeaconSpeech("Beacon", "Hanoi");
 const driver = new Builder()
   .forBrowser("chrome")
   .setChromeService(serviceBuilder)
   .build();
 
-
-
-excute_intent("play_music", listenToMusic(driver));
+execute_intent("play_music", listenToMusic(driver));
+const { start, register } = require(path.join(__dirname, "start.js"));
+const beacon = new BeaconSpeech("Beacon", "Hanoi");
 
 setTimeout(() => {
-  excute_intent("next_content", listenToMusic(driver));
+  execute_intent("next_content", listenToMusic(driver));
 }, 20000);
 
-// const beaconVolume = createBeaconVolume().then((result) => result);
-// const listenToMusicWithDriver = listenToMusic(driver);
-// const readNews = new ReadNewsController(driver);
-// const searchNewsBy = readNews.search.bind(readNews);
-// const selectOneToRead = readNews.selectOneToRead.bind(readNews);
-// a
-// contextBridge.exposeInMainWorld("electron", {
-// backgroundListen: beacon.backgroundListen.bind(beacon),
-// stopBackgroundListen: beacon.stopBackgroundListen.bind(beacon),
-// beaconVolume,
-// listenToMusic: listenToMusicWithDriver,
-// readNews: { searchNewsBy, selectOneToRead },
-// getAudioDevices,
-// });
+
+process.env.API_URL = "http://localhost:8000/api";
+
+// const init = async () => {
+//   await textToSpeech("Xin chào, tôi là Beacon, tôi có thể giúp gì cho bạn?");
+//   start()
+//     .then(async (res) => {
+//       if (res) {
+//         console.log("exist");
+//         return true;
+//       } else {
+//         await textToSpeech("Hãy đăng ký thông tin của bạn");
+//         await textToSpeech("Nhập tên");
+//         const name = await beacon.recognizeFromMicrophone();
+//         await textToSpeech("Nhập số điện thoại");
+//         const phone = await beacon.recognizeFromMicrophone();
+
+//         const userInfo = {
+//           name,
+//           phone,
+//         };
+
+//         return register(userInfo);
+//       }
+//     })
+//     .then((res) => {
+//       const beaconVolume = createBeaconVolume().then((result) => result);
+//       const listenToMusicWithDriver = listenToMusic(driver);
+//       const readNews = new ReadNewsController(driver);
+//       const searchNewsBy = readNews.search.bind(readNews);
+//       const selectOneToRead = readNews.selectOneToRead.bind(readNews);
+
+//       contextBridge.exposeInMainWorld("electron", {
+//         backgroundListen: beacon.backgroundListen.bind(beacon),
+//         stopBackgroundListen: beacon.stopBackgroundListen.bind(beacon),
+//         beaconVolume,
+//         listenToMusic: listenToMusicWithDriver,
+//         readNews: { searchNewsBy, selectOneToRead },
+//         getAudioDevices,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+// init();
