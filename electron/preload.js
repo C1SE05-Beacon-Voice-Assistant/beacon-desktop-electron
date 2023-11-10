18e54e38f0bd95780e5628aec2c14ddb0995da9e
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-11-11 02:03:37 +0700

    resolve conflict

--- electron/preload.js
+++ electron/preload.js
@@ -7,7 +7,7 @@ const chromedriverPath = require("chromedriver").path.replace(
 );
 const path = require("path");
 const { ServiceBuilder } = require("selenium-webdriver/chrome");
-const { excute_intent } = require("./excute_intent");
+const listen_to_music_excute_intent = require("./listen_to_music_excute_intent");
 // const BeaconSpeech = require(path.join(__dirname, "beacon_speech.js"));
 // const createBeaconVolume = require(path.join(__dirname, "control_volume.js"));
 const listenToMusic = require(path.join(__dirname, "listen_to_music.js"));
@@ -29,25 +29,64 @@ const driver = new Builder()
   .setChromeService(serviceBuilder)
   .build();
 
+const listenToMusicWithDriver = listenToMusic(driver);
+listen_to_music_excute_intent("play_music", listenToMusicWithDriver, "anh nho em nguoi yeu cu");
+// const executeException = require(path.join(__dirname, "situation_except.js"));
+// const textToSpeech = require(path.join(__dirname, "text_to_speech.js"));
+// const { start, register } = require(path.join(__dirname, "start.js"));
+// const beacon = new BeaconSpeech("Beacon", "Hanoi");
+
+// process.env.API_URL = "http://localhost:8000/api";
+
+// const init = () => {
+//   checkInternetConnection(async (isConnected) => {
+//     if (isConnected) {
+//       console.error("Máy tính đang kết nối internet.");
+//     } else {
+//       console.log("Máy tính không có kết nối internet.");
+//       executeException("noInternet");
+//     }
+//     // await textToSpeech("Xin chào, tôi là Beacon, tôi có thể giúp gì cho bạn?");
+//     start()
+//       .then(async (res) => {
+//         if (res) {
+//           console.log("exist");
+//           return true;
+//         } else {
+//           await textToSpeech("Hãy đăng ký thông tin của bạn");
+//           await textToSpeech("Nhập tên");
+//           const name = await beacon.recognizeFromMicrophone();
+//           await textToSpeech("Nhập số điện thoại");
+//           const phone = await beacon.recognizeFromMicrophone();
+//           const userInfo = {
+//             name,
+//             phone,
+//           };
+//           return register(userInfo);
+//         }
+//       })
+//       .then((res) => {
+//         console.log(res);
 
 
-excute_intent("play_music", listenToMusic(driver));
+//         const beaconVolume = createBeaconVolume().then((result) => result);
+//         const listenToMusicWithDriver = listenToMusic(driver);
+//         const searchNewsBy = readNews.search.bind(readNews);
+//         const selectOneToRead = readNews.selectOneToRead.bind(readNews);
 
-setTimeout(() => {
-  excute_intent("next_content", listenToMusic(driver));
-}, 20000);
+//         contextBridge.exposeInMainWorld("electron", {
+//           backgroundListen: beacon.backgroundListen.bind(beacon),
+//           stopBackgroundListen: beacon.stopBackgroundListen.bind(beacon),
+//           beaconVolume,
+//           listenToMusic: listenToMusicWithDriver,
+//           readNews: { searchNewsBy, selectOneToRead },
+//           getAudioDevices,
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   });
+// };
 
-// const beaconVolume = createBeaconVolume().then((result) => result);
-// const listenToMusicWithDriver = listenToMusic(driver);
-// const readNews = new ReadNewsController(driver);
-// const searchNewsBy = readNews.search.bind(readNews);
-// const selectOneToRead = readNews.selectOneToRead.bind(readNews);
-// a
-// contextBridge.exposeInMainWorld("electron", {
-// backgroundListen: beacon.backgroundListen.bind(beacon),
-// stopBackgroundListen: beacon.stopBackgroundListen.bind(beacon),
-// beaconVolume,
-// listenToMusic: listenToMusicWithDriver,
-// readNews: { searchNewsBy, selectOneToRead },
-// getAudioDevices,
-// });
+// init();
