99698dba095230bb6ff36b6352b36ba926343df8
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-11-22 12:02:31 +0700

    Completed create listen to music for user manual

---
+++ electron/helpers/listen_to_music_guide.js
@@ -0,0 +1,29 @@
+const listen_to_music_guide = {
+  introduction:
+    "Giới thiệu: Beacon là ứng dụng trợ lý ảo được tạo ra với mục đích hỗ trợ người khiếm thị trong việc sử dụng máy tính cá nhân một cách dễ dàng. Với Beacon, ứng dụng sẽ giúp người khiếm thị truy cập internet để nghe nhạc và đọc báo một cách thuận tiện và thoải mái.",
+  requirements:
+    "Yêu cầu: Để có trải nghiệm tốt nhất, bạn nên có micro hoặc ở một nơi yên tĩnh để Beacon có thể nhận diện giọng nói và lọc tạp âm hiệu quả nhất. Máy tính cần được kết nối với internet.",
+  play_music:
+    "Phát nhạc: để phát nhạc, hãy nói 'Phát nhạc' hoặc 'Nghe nhạc'. Beacon sẽ tìm và phát bài hát 'Anh nhớ em người yêu cũ' cho bạn.",
+  stop_content:
+    "Dừng nội dung: để dừng nội dung đang phát, hãy nói 'Dừng' hoặc 'Tạm dừng'.",
+  resume_content:
+    "Tiếp tục nội dung: để tiếp tục phát nội dung sau khi đã tạm dừng, hãy nói 'Tiếp tục' hoặc 'Phát tiếp'.",
+  increase_volume:
+    "Tăng âm lượng: để tăng âm lượng, hãy nói 'Tăng âm lượng' hoặc 'Tăng tiếng'.",
+  decrease_volume:
+    "Giảm âm lượng: để giảm âm lượng, hãy nói 'Giảm âm lượng' hoặc 'Giảm tiếng'.",
+  min_volume:
+    "Âm lượng nhỏ nhất: để đặt âm lượng thành nhỏ nhất, hãy nói 'Âm lượng nhỏ nhất' hoặc 'Tắt tiếng'.",
+  max_volume:
+    "Âm lượng lớn nhất: để đặt âm lượng thành lớn nhất, hãy nói 'Âm lượng lớn nhất' hoặc 'Bật tiếng'.",
+  mute: "Tắt tiếng: để tắt tiếng, hãy nói 'Tắt tiếng' hoặc 'Mất tiếng'.",
+  un_mute:
+    "Bật tiếng: để bật tiếng sau khi đã tắt tiếng, hãy nói 'Bật tiếng' hoặc 'Quay lại tiếng'.",
+  set_volume:
+    "Đặt âm lượng: để đặt mức âm lượng mong muốn, hãy nói 'Đặt âm lượng' kèm theo số lượng. Ví dụ: 'Đặt âm lượng 20'.",
+  next_content:
+    "Nội dung tiếp theo: để chuyển sang nội dung tiếp theo, hãy nói 'Nội dung tiếp theo' hoặc 'Bài tiếp theo'.",
+};
+
+module.exports = listen_to_music_guide;
